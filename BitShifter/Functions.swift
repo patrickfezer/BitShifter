7dfbd649cfa9cc1a0d036dbedfa1b0e829209d97
Patrick Fezer <p.fezer@gmx.de>
2022-08-21 09:25:37 +0200

    added description to functions file

--- BitShifter/Functions.swift
+++ BitShifter/Functions.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 // MARK: Functions
+
+// Retuns a Text view with leading alignment
 func LeadingText(_ input: String) -> some View {
     return Text(input).frame(width: nil, height: nil, alignment: .leading)
 }
 
+// Convert an String to Integer
 func StringToInt(_ input: String) -> Int {
     return Int(input) ?? 0
 }
